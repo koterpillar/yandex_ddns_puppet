b2336cc3dc6208225571b4b0dcab6998aaf190a1
Alexey Kotlyarov <a@koterpillar.com>
2019-04-12 21:36:10 +1000

    Fix validating root domain record

--- files/cleanup.sh
+++ files/cleanup.sh
@@ -4,9 +4,14 @@ set -e
 
 . $(dirname $0)/functions.sh
 
-SUBDOMAIN=$(echo $CERTBOT_DOMAIN | sed -E 's/\..+//g')
-
-RECORD=_acme-challenge.$SUBDOMAIN
+if [ "$CERTBOT_DOMAIN" = "$DOMAIN" ]
+then
+    SUBDOMAIN=
+    RECORD=_acme-challenge
+else
+    SUBDOMAIN=$(echo $CERTBOT_DOMAIN | sed -E 's/\..+//g')
+    RECORD=_acme-challenge.$SUBDOMAIN
+fi
 
 existing=$(get_record_id TXT $RECORD)
 if [ -n "$existing" ]
